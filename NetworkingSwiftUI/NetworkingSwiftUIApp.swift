5cb365df02017ce7c4f996fcd59282c17d368acd
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-11 17:09:28 +0300

    Initial Commit

---
+++ NetworkingSwiftUI/NetworkingSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingSwiftUIApp.swift
+//  NetworkingSwiftUI
+//
+//  Created by Yunus Emre Taşdemir on 11.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
