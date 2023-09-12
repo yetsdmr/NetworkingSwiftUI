7412ec74ca48e2c76bd172f3a004f4de7d07996c
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-12 16:41:32 +0300

    Parsing JSON.

--- NetworkingSwiftUI/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ NetworkingSwiftUI/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -12,17 +12,31 @@ class CoinsViewModel: ObservableObject{
     @Published var price = ""
     
     init() {
-        fetchPrice()
+        fetchPrice(coin: "bitcoin")
     }
     
-    func fetchPrice() {
-        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids-bitcoin&vs_currencies=usd"
+    func fetchPrice(coin: String) {
+        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
         guard let url = URL(string: urlString) else { return }
         
         print( "Fetching price..")
         
         URLSession.shared.dataTask(with: url) { data, response, error in
             print("Did receive data \(data)")
+            guard let data = data else { return }
+            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+            print(jsonObject)
+            guard let value = jsonObject[coin] as? [String: Double] else {
+                print("Failed to parse value")
+                return
+            }
+            guard let price = value[ "usd"] else { return }
+            
+            DispatchQueue.main.async {
+                self.coin = coin.capitalized
+                self.price = "$\(price)"
+            }
+            
         }.resume()
         
         print("Did reach end of function..")
