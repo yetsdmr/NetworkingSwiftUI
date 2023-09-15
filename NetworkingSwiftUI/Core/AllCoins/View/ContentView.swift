1e74902ae1958bc10aa0c5763a5a9d22c449cd20
Yunus Emre Taşdemir <yetsdmr@gmail.com>
2023-09-15 16:36:43 +0300

    Added custom decoding and error handling.

--- NetworkingSwiftUI/Core/AllCoins/View/ContentView.swift
+++ NetworkingSwiftUI/Core/AllCoins/View/ContentView.swift
@@ -13,7 +13,23 @@ struct ContentView: View {
     var body: some View {
         List {
             ForEach(viewModel.coins) { coin in
-                Text(coin.name)
+                HStack(spacing: 12 ) {
+                    Text("\(coin.marketCapRank)")
+                        .foregroundColor(.gray)
+                    
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text(coin.name)
+                            .fontWeight(.semibold)
+                        
+                        Text(coin.symbol.uppercased())
+                    }
+                    .font(.footnote)
+                }
+            }
+        }
+        .overlay {
+            if let error = viewModel.errorMessage {
+                Text(error)
             }
         }
     }
--- NetworkingSwiftUI/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ NetworkingSwiftUI/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -10,18 +10,38 @@ import SwiftUI
 class CoinsViewModel: ObservableObject{
     
     @Published var coins = [Coin]()
+    @Published var errorMessage: String?
     
     private let service = CoinDataService()
     
     init() {
         //fetchPrice(coin: "bitcoin")
-        fetchCoins()
+        //fetchCoins()
+        fetchCoinsWithResult()
     }
     
     func fetchCoins() {
-        service.fetchCoins { coins in
+        service.fetchCoins { coins, error in
             DispatchQueue.main.async {
-                self.coins = coins
+                if let error = error {
+                    self.errorMessage = error.localizedDescription
+                    return 
+                }
+                 
+                self.coins = coins ?? []
+            }
+        }
+    }
+    
+    func fetchCoinsWithResult() {
+        service.fetchCoinsWithResult { result in
+            DispatchQueue.main.async {
+                switch result {
+                case .success(let coins):
+                    self.coins = coins
+                case .failure(let error):
+                    self.errorMessage = error.localizedDescription
+                }
             }
         }
     }
